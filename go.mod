faeb7ca43812a32a89b5e650605622d524adffe1
Danila <danila@x.com>
2025-02-06 01:28:48 +0300

    start valorant content

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DanilAiro/go-project-statistics
+
+go 1.23.3
---
+++ valorant_content.go
@@ -0,0 +1,20 @@
+package valorant_content
+
+type Weapon struct {
+	Name string
+}
+
+type Map struct {
+	Name string
+
+}
+
+type Agent struct {
+	Name string
+}
+
+type Ability struct {
+	Name string
+	Agent Agent
+}
+
